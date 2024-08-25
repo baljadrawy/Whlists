0c92c3109cbec3bff299a5d0323ea19dce8ce666
baljadrawy <157516849+baljadrawy@users.noreply.github.com>
2024-08-25 15:31:41 +0300

    Update Wishlist.js

--- models/Wishlist.js
+++ models/Wishlist.js
@@ -2,14 +2,7 @@ const mongoose = require('mongoose');
 
 const WishlistSchema = new mongoose.Schema({
     userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-    items: [
-        {
-            title: { type: String, required: true },
-            image: { type: String },
-            description: { type: String },
-            isFulfilled: { type: Boolean, default: false },
-        },
-    ],
+    items: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Item' }],
 });
 
 const Wishlist = mongoose.model('Wishlist', WishlistSchema);
