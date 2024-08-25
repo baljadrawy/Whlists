52262ecd94e51a45d6134c88a21cac4e3e34f624
baljadrawy <157516849+baljadrawy@users.noreply.github.com>
2024-08-25 15:32:57 +0300

    Update scripts.js

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,22 +1,3 @@
-document.getElementById('login-form').addEventListener('submit', async (e) => {
-    e.preventDefault();
-
-    const email = document.getElementById('login-email').value;
-    const password = document.getElementById('login-password').value;
-
-    const response = await fetch('/api/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password }),
-    });
-
-    const data = await response.json();
-    if (response.ok) {
-        localStorage.setItem('token', data.token); // Store the token in localStorage
-        window.location.href = 'wishlists.html'; // Redirect to wishlists page
-    } else {
-        alert('Login failed: ' + data.error);
-    }
+document.addEventListener('DOMContentLoaded', function() {
+    console.log("Page Loaded");
 });
